0fd7455e60413c3f47959108a5e05277f28566a8
Nico Albanese <gcalbanese96@gmail.com>
2021-04-06 21:18:37 +0100

    changes for lect1-2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { AuthProvider } from "@/lib/auth";
+
+import { ThemeProvider, CSSReset } from "@chakra-ui/react";
+import theme from "@/styles/theme";
+
+// if i want to add the global styles thing its at 53:00 of react2025-firestore/chakra
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,41 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Button, Heading, Text, Code, Flex } from "@chakra-ui/react";
+import { SunIcon } from "@chakra-ui/icons";
+import Head from "next/head";
+import { useAuth } from "@/lib/auth";
 
 export default function Home() {
+  const auth = useAuth();
+
   return (
-    <div className={styles.container}>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      maxW="300px"
+      w="full"
+      mx="auto"
+      h="100vh"
+    >
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Fast Feedback
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <SunIcon w="30" h="30" color="red.700" />
+      <Text textAlign="center">
+        Current user: <Code>{auth.user ? auth.user.email : "None"}</Code>
+      </Text>
+      {!auth?.user ? (
+        <Button
+          mt={4}
+          size="sm"
+          onClick={(e) => auth.signinWithGithub()}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+          Sign In
+        </Button>
+      ) : (
+        <Button mt={4} onClick={() => auth.signout()}>Sign Out</Button>
+      )}
+    </Flex>
+  );
 }
