c7fe2b02f6dbf76b281af5b323cde10c08a209a8
Nico Albanese <gcalbanese96@gmail.com>
2021-04-07 19:21:23 +0100

    fixed login dashboard issue

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,14 +5,43 @@ import SiteTableSkeleton from "@/components/SiteTableSkeleton";
 import useSWR from "swr";
 import fetcher from "utils/fetcher";
 import SiteTable from "@/components/SiteTable";
+import { Flex, Heading, Text } from "@chakra-ui/layout";
+import Link from "next/link";
 
 const Dashboard = () => {
   const auth = useAuth();
 
   const { data } = useSWR("/api/sites", fetcher);
   console.log(data);
+  console.log(auth);
 
-  console.log(auth.user);
+  if (auth.user == false) {
+    return (
+      <Flex
+        h='100vh'
+        w='full'
+        bg='gray.100'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Heading fontSize='2xl'>
+          Please login{" "}
+          <Link href='/'>
+            <Text
+              display='inline-block'
+              color='blue.300'
+              textDecoration='underline'
+              cursor="pointer"
+              _hover={{color:"blue.700"}}
+            >
+              here
+            </Text>
+          </Link>
+          .
+        </Heading>
+      </Flex>
+    );
+  }
   if (auth.loading) {
     return (
       <DashboardShell>
@@ -31,7 +60,11 @@ const Dashboard = () => {
 
   return (
     <DashboardShell>
-      {data ? <SiteTable sites={data.sites} /> : <SiteTableSkeleton />}
+      {auth.user && data ? (
+        <SiteTable sites={data.sites} />
+      ) : (
+        <SiteTableSkeleton />
+      )}
     </DashboardShell>
   );
 };
